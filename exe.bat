6b6fa181e7d9e97cfc5cf77d85d57dc49ebd16b6
polfes <glushkovin2000@gmail.com>
2026-06-08 17:43:22 +0300

    Rename exe.bat to create exe.bat

--- exe.bat
+++
@@ -1,1 +0,0 @@
-pyinstaller --onefile --name "TextureConverter" --add-data "NVIDIA Texture Tools;NVIDIA Texture Tools" --console --uac-admin --icon="icon.ico" --version-file="version.txt" texture_converter.py
